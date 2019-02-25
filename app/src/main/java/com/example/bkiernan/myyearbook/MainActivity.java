a037ed2e5afaa810939f713549b96440e5185ae5
Brendan <brendank196@gmail.com>
2019-02-25 00:13:07 -0500

    Added WebView, and Button to interact with it.

--- app/src/main/java/com/example/bkiernan/myyearbook/MainActivity.java
+++ app/src/main/java/com/example/bkiernan/myyearbook/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.bkiernan.myyearbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +19,14 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView imageView = findViewById(R.id.imageView);
         imageView.setImageResource(R.drawable.github_prof);
+
+        webView = findViewById(R.id.webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true); //XSS shouldn't be an issue given the hard-coded URL
+    }
+
+    public void loadWebView(View view) {
+        webView.loadUrl("https://www.google.com/");
     }
 }
